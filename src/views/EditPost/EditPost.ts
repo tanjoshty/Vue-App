f3316888a7e9bbecfa52b00aedb69ac303f7b11f
tanjoshty <tanjoshty@gmail.com>
2021-10-26 13:13:23 +1100

    Edit post working

--- src/views/EditPost/EditPost.ts
+++ src/views/EditPost/EditPost.ts
@@ -6,17 +6,45 @@ import router from '@/router';
 export default class EditPost extends Vue {
     public title: string = '';
     public body: string = '';
+    private postId: any;
+    private currentUser: any = store.state.user.username;
+    private isLoading: boolean = true;
+    private post: any = {};
 
-    public async createPost(): Promise<void> {
+    mounted() {
+        this.postId = this.$route.params.id;
+        this.fetchPost(this.postId);
+    }
+
+    public goToPost(id: string): void {
+        router.push({name: 'singlePost', params: {id}})
+    }
+
+    public async fetchPost(id: string): Promise<void> {
+        try {
+            const response = await Axios.get(`/post/${id}`);
+            this.title = response.data.title;
+            this.body = response.data.body;
+            this.isLoading = false;
+            console.log(response.data);
+            if (this.currentUser != response.data.author.username) {
+                console.log("No Permissions");
+                router.push({name: 'home'});
+            }
+        } catch (error) {
+            console.log("There was a problem");
+        }
+    }
+
+    public async updatePost(): Promise<void> {
         const title = this.title;
         const body = this.body;
 
         try {
-            // API Call to register
-            const response = await Axios.post("/create-post", { title, body, token: store.state.user.token });
-            console.log("Post was successfully created");
+            const response = await Axios.post(`/post/${this.postId}/edit`, { title, body, token: store.state.user.token });
+            console.log("Post was successfully updated");
             console.log(response.data)
-            router.push({name: 'home'});
+            this.goToPost(this.postId);
             
         } catch (error) {
             console.log("There was a problem");
